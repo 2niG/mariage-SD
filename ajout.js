09a0f800b829ea5db6c586cbb1ed19666688df11
2niG <168422030+2niG@users.noreply.github.com>
2024-06-05 13:37:38 +0200

    Update ajout.js

--- ajout.js
+++ ajout.js
@@ -1,9 +1,8 @@
 // Définir la date et l'heure de fin
-var compteAReboursDate = new Date('2025-06-07T00:00:00').getTime() ; // 10 jours à partir de maintenant
+var compteAReboursDate = new Date("2025-06-07T00:00:00").getTime(); // 10 jours à partir de maintenant
 
 // Mettre à jour le compte à rebours toutes les secondes
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Obtenir la date et l'heure actuelles
   var maintenant = new Date().getTime();
 
@@ -12,17 +11,40 @@ var x = setInterval(function() {
 
   // Calculer le temps pour les jours, heures, minutes et secondes
   var jours = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var heures = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var heures = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var secondes = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Afficher le résultat dans un élément avec l'ID "compteARebours"
-  document.getElementById("compteARebours").innerHTML = jours + "j " + heures + "h "
-  + minutes + "m " + secondes + "s ";
+  document.getElementById("compteARebours").innerHTML =
+    jours + "j " + heures + "h " + minutes + "m " + secondes + "s ";
 
-  // Si le compte à rebours est terminé, écrire du texte 
+  // Si le compte à rebours est terminé, écrire du texte
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("compteARebours").innerHTML = "EXPIRÉ";
   }
 }, 1000);
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+document.querySelector(".retour-button").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
